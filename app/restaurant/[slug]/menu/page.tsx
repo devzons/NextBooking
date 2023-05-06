97ea308c2753c39724cb517d7a7f439913c80294
Don Yu <37088038+devzons@users.noreply.github.com>
2023-05-06 11:40:31 -0700

    fetching the restaurant menu

--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,16 +1,40 @@
 import type { Metadata } from 'next'
+import { PrismaClient } from '@prisma/client'
 import { RestaurantNavBar, Menu } from '../components'
+
+const prisma = new PrismaClient()
+
+const fetchRestaurantMenu = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: { slug },
+    select: {
+      items: true,
+    },
+  })
+
+  if (!restaurant) {
+    throw new Error()
+  }
+
+  return restaurant.items
+}
 
 export const metadata: Metadata = {
   title: 'menu | restaurant',
   description: 'Menu page',
 }
 
-export default function RestaurantMenu() {
+export default async function RestaurantMenu({
+  params,
+}: {
+  params: { slug: string }
+}) {
+  const menu = await fetchRestaurantMenu(params.slug)
+
   return (
     <div className='bg-white w-[100%] rounded p-3 shadow'>
-      <RestaurantNavBar />
-      <Menu />
+      <RestaurantNavBar slug={params.slug} />
+      <Menu menu={menu} />
     </div>
   )
 }
