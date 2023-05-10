7c63e2dd803cea8bef63b27a3e09d476f5f0247d
Don Yu <37088038+devzons@users.noreply.github.com>
2023-05-10 00:19:39 -0700

    conditional render UI based on the Auth state

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,7 +1,15 @@
+'use client'
+
 import Link from 'next/link'
 import AuthModal from './AuthModal'
+import { useContext } from 'react'
+import { AuthenticationContext } from '../context/AuthContext'
+import useAuth from '../../hooks/useAuth'
 
 export default function NavBar() {
+  const { data, loading } = useContext(AuthenticationContext)
+  const { signout } = useAuth()
+
   return (
     <nav className='bg-white p-2 flex justify-between'>
       <Link href='/' className='text-indigo-800 text-2xl'>
@@ -10,10 +18,23 @@ export default function NavBar() {
         </span>
       </Link>
       <div>
-        <div className='flex'>
-          <AuthModal isSignin={true} />
-          <AuthModal isSignin={false} />
-        </div>
+        {loading ? null : (
+          <div className='flex'>
+            {data ? (
+              <button
+                className='bg-indigo-700 text-white border p-1 px-4 rounded mr-3'
+                onClick={signout}
+              >
+                Sign out
+              </button>
+            ) : (
+              <>
+                <AuthModal isSignin={true} />
+                <AuthModal isSignin={false} />
+              </>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   )
