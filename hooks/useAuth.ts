849eb5f7933cdb1b54d1010fa175603fee9c9c68
Don Yu <37088038+devzons@users.noreply.github.com>
2023-05-09 23:26:27 -0700

    Save JWT to cookie

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,13 +1,26 @@
 import axios from 'axios'
+import { getCookie, removeCookies } from 'cookies-next'
+import { useContext } from 'react'
+import { AuthenticationContext } from '../app/context/AuthContext'
 
 const useAuth = () => {
-  const signin = async ({
-    email,
-    password,
-  }: {
-    email: string
-    password: string
-  }) => {
+  const { setAuthState } = useContext(AuthenticationContext)
+
+  const signin = async (
+    {
+      email,
+      password,
+    }: {
+      email: string
+      password: string
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    })
     try {
       const response = await axios.post(
         'http://localhost:3000/api/auth/signin',
@@ -16,15 +29,85 @@ const useAuth = () => {
           password,
         }
       )
-      console.log(response)
-    } catch (error) {
-      console.log(error)
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      })
+      handleClose()
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      })
+    }
+  }
+  const signup = async (
+    {
+      email,
+      password,
+      firstName,
+      lastName,
+      city,
+      phone,
+    }: {
+      email: string
+      password: string
+      firstName: string
+      lastName: string
+      city: string
+      phone: string
+    },
+    handleClose: () => void
+  ) => {
+    setAuthState({
+      data: null,
+      error: null,
+      loading: true,
+    })
+    try {
+      const response = await axios.post(
+        'http://localhost:3000/api/auth/signup',
+        {
+          email,
+          password,
+          firstName,
+          lastName,
+          city,
+          phone,
+        }
+      )
+      setAuthState({
+        data: response.data,
+        error: null,
+        loading: false,
+      })
+      handleClose()
+    } catch (error: any) {
+      setAuthState({
+        data: null,
+        error: error.response.data.errorMessage,
+        loading: false,
+      })
     }
   }
 
-  const signup = async () => {}
+  const signout = () => {
+    removeCookies('jwt')
 
-  return { signin, signup }
+    setAuthState({
+      data: null,
+      error: null,
+      loading: false,
+    })
+  }
+
+  return {
+    signin,
+    signup,
+    signout,
+  }
 }
 
 export default useAuth
